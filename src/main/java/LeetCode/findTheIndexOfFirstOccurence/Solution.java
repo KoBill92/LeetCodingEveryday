3d6eefe4763075d3ff16cca07234980931a7b2b4
Kobil Bill <1810092kk@gmail.com>
2023-05-13 12:09:54 +0500

    solved index of first occurence in the given string

--- src/main/java/LeetCode/findTheIndexOfFirstOccurence/Solution.java
+++ src/main/java/LeetCode/findTheIndexOfFirstOccurence/Solution.java
@@ -1,5 +1,18 @@
 package LeetCode.findTheIndexOfFirstOccurence;
 
-public class Solution {
+import java.util.HashMap;
 
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(strStr("sadbutsad", "sad"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        if (needle.length() > haystack.length()) return -1;
+        if (haystack.contains(needle)) {
+            int index = haystack.indexOf(needle);
+            return index;
+        }
+        return -1;
+    }
 }
